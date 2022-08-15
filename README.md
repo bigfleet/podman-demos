06eee48418aa698e56a2ab48bb39b576b4a6592f
Jim Van Fleet <jim.van.fleet@levvel.io>
2022-08-15 14:25:40 -0400

    Getting started with springboot-api

--- README.md
+++ README.md
@@ -13,3 +13,10 @@ podman build -t eagle/runner ansible
 ```
 podman build -t eagle/sops sops
 ```
+
+```
+podman build \
+  --build-arg PARAM_APP_NAME=zimcore \
+  --build-arg PARAM_PRIMARY_DOMAIN=bigfleet.dev \
+  -t eagle/springboot-api springboot-api
+```
---
+++ springboot-api/src/boot.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+pkg=`tr '.' $'\n' <<< "$PARAM_PRIMARY_DOMAIN" | tac | paste -s -d '.'`
+name=`cat $1 | jq -r .name`
+defaultPackages=("oauth2-client" "actuator" "webflux" "web")
+
+if [ `cat $1 | jq '.services | map(select(.=="mongodb")) | length'` -ge "1" ]; then
+  defaultPackages+=("data-mongodb")
+fi
+if [ `cat $1 | jq '.services | map(select(.=="rabbitmq")) | length'` -ge "1" ]; then
+  defaultPackages+=("amqp")
+fi
+
+pkgs=$(IFS=, ; echo "${defaultPackages[*]}")
+
+url="https://start.spring.io/starter.zip?type=gradle-project&baseDir=init&language=java&bootVersion=$PARAM_SPRINGBOOT_VERSION&groupId=$pkg&artifactId=$name&name=$name&description=Demo%20project%20for%20$name&packageName=$pkg.$name&packaging=jar&javaVersion=$PARAM_JAVA_VERSION&dependencies=$pkgs"
+`curl -o /home/eagle/project.zip $url`
+#unzip /home/eagle/project.zip
+#rm /home/eagle/project.zip
